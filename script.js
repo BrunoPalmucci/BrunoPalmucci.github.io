b917638fd3199fc3a79c54bc0c0d4d09c0478b1b
BrunoPalmucci <brunoapalmucci@sanluis.edu.ar>
2023-10-29 20:01:57 -0300

    Update form
    Se corrigieron errores en form y se agregaron imagenes para el carrousel(en proceso).

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 function validarFormulario() {
-    let div = document.getElementById("frm");
-    let mail = document.forms["form_contacto"]["mail"].value;
-    let nombre = document.forms["form_contacto"]["nombre"].value;
-    let tiene_error = false;
+    let div = document.getElementById("frm")
+    let mail = document.forms["form_contacto"]["mail"].value
+    let nombre = document.forms["form_contacto"]["nombre"].value
+    let tiene_error = false
 
     if (mail == "") {
-        document.getElementById("errorMail").innerHTML = "Campo de correo vacío";
-        tiene_error = true;
-    }else document.getElementById("errorMail").innerHTML = "";
+        document.getElementById("errorMail").innerHTML = "Campo de correo vacío"
+        tiene_error = true
+    }else document.getElementById("errorMail").innerHTML = ""
 
     if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))) {
-        document.getElementById("errorMail").innerHTML = "Mail inválido";
-        tiene_error = true;
-    }else document.getElementById("errorMail").innerHTML = "";
+        document.getElementById("errorMail").innerHTML = "Mail inválido"
+        tiene_error = true
+    }else document.getElementById("errorMail").innerHTML = ""
 
     if (nombre == "") {
-        document.getElementById("errorNombre").innerHTML = "Campo vacío";
-        tiene_error = true;
-    }else document.getElementById("errorNombre").innerHTML = "";
+        document.getElementById("errorNombre").innerHTML = "Campo vacío"
+        tiene_error = true
+    }else document.getElementById("errorNombre").innerHTML = ""
 
     if (nombre.length > 50) {
-        document.getElementById("errorNombre").innerHTML = "Nombre muy largo";
-        tiene_error = true;
-    }else document.getElementById("errorNombre").innerHTML = "";
+        document.getElementById("errorNombre").innerHTML = "Nombre muy largo"
+        tiene_error = true
+    }else document.getElementById("errorNombre").innerHTML = ""
 
     if (/\d/.test(nombre) || !/^[A-Za-z]+$/.test(nombre)) {
-        document.getElementById("errorNombre").innerHTML = "Su nombre solo puede contener letras y no debe tener números ni otros caracteres";
-        tiene_error = true;
-    }else document.getElementById("errorNombre").innerHTML = "";
+        document.getElementById("errorNombre").innerHTML = "Su nombre solo puede contener letras y no debe tener números ni otros caracteres"
+        tiene_error = true
+    }else document.getElementById("errorNombre").innerHTML = ""
 
-    if(!tiene_error){
-      alert("Inscripto con exito!")
+    if (!tiene_error) {
+        let exito = document.getElementById("exito")
+        if (exito) {
+            exito.remove()
+        }
+
+        exito = document.createElement("div")
+        exito.id = "exito"
+        exito.className = "exito"
+        exito.innerHTML = "Bienvenido a la pagina, " + nombre + "! Ingresaste con el mail: " + mail
+        div.appendChild(exito)
     }
-    return !tiene_error;
+    return false
 }
